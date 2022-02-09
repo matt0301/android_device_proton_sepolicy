65e43e579cd4760e4619929e81f96b2d37142547
Anay Wadhera <anay1018@gmail.com>
2022-02-09 17:43:48 +0900

    sepolicy: include flipendo/turbo_adapter policy
    Change-Id: I92687a3a4e8a760df764d77f09e1dab96de6e388
    Reviewed-on: https://review.statixos.com/c/android_device_statix_sepolicy/+/6852
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -24,11 +24,15 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/proton/sepolicy/common/dynamic \
-    device/proton/sepolicy/common/system
+    device/proton/sepolicy/common/system \
+    hardware/google/pixel-sepolicy/flipendo \
+    hardware/google/pixel-sepolicy/turbo_adapter
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/proton/sepolicy/common/dynamic \
-    device/proton/sepolicy/common/vendor
+    device/proton/sepolicy/common/vendor \
+    hardware/google/pixel-sepolicy/flipendo \
+    hardware/google/pixel-sepolicy/turbo_adapter
 endif
 
 # Selectively include legacy rules defined by the products
